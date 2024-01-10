9ede79c34eb27b278a373dfbd2420f1f1394a2ee
Freddie Nelson <47699795+freddie-nelson@users.noreply.github.com>
2024-01-10 20:45:34 +0000

    chapter 6

--- populate_rango.py
+++ populate_rango.py
@@ -14,20 +14,20 @@ def populate():
     # through each data structure, and add the data to our models.
 
     python_pages = [
-        {'title': 'Official Python Tutorial', 'url':'http://docs.python.org/3/tutorial/'},
-        {'title':'How to Think like a Computer Scientist', 'url':'http://www.greenteapress.com/thinkpython/'},
-        {'title':'Learn Python in 10 Minutes', 'url':'http://www.korokithakis.net/tutorials/python/'} 
+        {'title': 'Official Python Tutorial', 'url':'http://docs.python.org/3/tutorial/', 'views': 36},
+        {'title':'How to Think like a Computer Scientist', 'url':'http://www.greenteapress.com/thinkpython/', 'views': 64},
+        {'title':'Learn Python in 10 Minutes', 'url':'http://www.korokithakis.net/tutorials/python/', 'views': 28} 
     ]
 
     django_pages = [
-        {'title':'Official Django Tutorial', 'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
-        {'title':'Django Rocks', 'url':'http://www.djangorocks.com/'},
-        {'title':'How to Tango with Django', 'url':'http://www.tangowithdjango.com/'} 
+        {'title':'Official Django Tutorial', 'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/', 'views': 20},
+        {'title':'Django Rocks', 'url':'http://www.djangorocks.com/', 'views': 31},
+        {'title':'How to Tango with Django', 'url':'http://www.tangowithdjango.com/', 'views': 13} 
     ]
 
     other_pages = [
-        {'title':'Bottle', 'url':'http://bottlepy.org/docs/dev/'},
-        {'title':'Flask', 'url':'http://flask.pocoo.org'} 
+        {'title':'Bottle', 'url':'http://bottlepy.org/docs/dev/', 'views': 9},
+        {'title':'Flask', 'url':'http://flask.pocoo.org', 'views': 23} 
     ]
 
     cats = {
@@ -45,7 +45,7 @@ def populate():
         c = add_cat(cat, cat_data)
 
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'], p['views'])
 
     # Print out the categories we have added.
     for c in Category.objects.all():
--- reset_db.bat
+++ reset_db.bat
@@ -1,5 +1,5 @@
 del db.sqlite3
 python manage.py makemigrations rango
 python manage.py migrate
-python manage.py createsuperuser --username freddie --email 
+python manage.py createsuperuser --username freddie --email freddie@freddienelson.co.uk
 python populate_rango.py
